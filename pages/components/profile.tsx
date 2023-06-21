92cb761a5a8bba14117ccbbfa573d98ea860a1c1
jaivantraja <125119970+jaivantraja@users.noreply.github.com>
2023-06-22 03:44:59 +0530

    Update profile.tsx

--- pages/components/profile.tsx
+++ pages/components/profile.tsx
@@ -6,7 +6,7 @@ export default function Profile()
 {
     return(
         <div className={styles.profile}>
-            <Image style={{paddingTop:'37px',userSelect:"none"}} title={"Jaivant"} src="/profile.jpg" alt="profile" width="1500" height="50"/>
+            <Image style={{paddingTop:'37px',userSelect:"none"}} title={"Jaivant"} src="./profile.jpg" alt="profile" width="1500" height="50"/>
         </div>
     )
 }
