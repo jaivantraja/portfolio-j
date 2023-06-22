fc54e2f1d3e4a2f1fc1f2c961631c0288c80d32e
jaivantraja <125119970+jaivantraja@users.noreply.github.com>
2023-06-22 10:43:13 +0530

    Update education.tsx

--- pages/components/education.tsx
+++ pages/components/education.tsx
@@ -15,12 +15,12 @@ export default function Education()
             <p className={rubik600.className} style={{fontSize:"38px"}}>Education</p>
             <div className={styles.timeline}>
                 <div className={styles.timelineItem}>
-                    <h3 className={styles.date}>2020 - 2025</h3>
-                    <h4 className={styles.degree}>BE Computer Science Engineering</h4>
+                    <h3 className={styles.date}>2021 - 2025</h3>
+                    <h4 className={styles.degree}>BE Computer Science Engineering <b>GPA OF 8.48/10</b></h4>
                     <p className={styles.school}>PSG Insitute Of Technology And Applied Research, Coimbatore, India</p>
                 </div>
                 <div className={styles.timelineItem}>
-                    <h3 className={styles.date}>2015 - 2020</h3>
+                    <h3 className={styles.date}>2015 - 2021</h3>
                     <h4 className={styles.degree}>Secondary School</h4>
                     <p className={styles.school}>The NGP School, Coimbatore, India</p>
                 </div>
